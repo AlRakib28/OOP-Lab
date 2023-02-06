02f2f6ed04df7be035ba534728e5743318e1b76d
Ahmed Al Rakib <101850752+AlRakib28@users.noreply.github.com>
2023-02-07 02:06:47 +0600

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,12 @@
+
+package ALL__Java;
+
+
+public class HW1 {
+    
+    public static void main(String[] args){
+     
+        System.out.println(" NAME:Ahmed Al Rakib\nAddress:Mirpur 1,Dhaka-1216\nDepartment:CSE\nFavourite game:Pubg");
+       
+    }
+}
---
+++ Print_Name.java
@@ -0,0 +1,10 @@
+
+package ALL__Java;
+
+
+public class Print_Name {
+ public static void main(String[] args){
+     String R ="Ahmed Al Rakib";
+     System.out.println(R);
+ }   
+}
